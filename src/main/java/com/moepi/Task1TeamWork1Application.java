ab5f92c354056fc3262fc060f13b6457069ab663
Daily-Technology <Daily34tech@gmail.com>
2024-09-17 06:49:33 +0200

    added the html to spring boot

---
+++ src/main/java/com/moepi/Task1TeamWork1Application.java
@@ -0,0 +1,14 @@
+package com.moepi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Task1TeamWork1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Task1TeamWork1Application.class, args);
+		System.out.println("Listening on port 8080");
+	}
+
+}
